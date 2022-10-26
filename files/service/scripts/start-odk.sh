65e83800da7f912ed9790b3f8281da1a126caf6f
alxndrsn <alxndrsn>
2022-10-26 12:54:20 +0000

    add timestamps

--- files/service/scripts/start-odk.sh
+++ files/service/scripts/start-odk.sh
@@ -1,27 +1,31 @@
+log() {
+  echo "$(date) [start-odk] $*"
+}
+
 CONFIG_PATH=/usr/odk/config/local.json
-echo "generating local service configuration.."
+log "generating local service configuration.."
 /bin/bash -c "ENKETO_API_KEY=$(cat /etc/secrets/enketo-api-key) envsubst '\$DOMAIN:\$HTTPS_PORT:\$SYSADMIN_EMAIL:\$ENKETO_API_KEY' < /usr/share/odk/config.json.template > $CONFIG_PATH"
 
 echo
-echo '----------------------------------------'
-echo 'CONFIG:'
+log '----------------------------------------'
+log 'CONFIG:'
 cat /usr/share/odk/config.json.template
-echo '----------------------------------------'
+log '----------------------------------------'
 echo
 
-echo "running migrations.."
+log "running migrations.."
 node -e 'const { withDatabase, migrate } = require("./lib/model/migrate"); withDatabase(require("config").get("default.database"))(migrate);'
 
-echo "checking migration success.."
+log "checking migration success.."
 node -e 'const { withDatabase, checkMigrations } = require("./lib/model/migrate"); withDatabase(require("config").get("default.database"))(checkMigrations);'
 
 if [ $? -eq 1 ]; then
-  echo "*** Error starting ODK! ***"
-  echo "After attempting to automatically migrate the database, we have detected unapplied migrations, which suggests a problem with the database migration step. Please look in the console above this message for any errors and post what you find in the forum: https://forum.getodk.org/"
+  log "*** Error starting ODK! ***"
+  log "After attempting to automatically migrate the database, we have detected unapplied migrations, which suggests a problem with the database migration step. Please look in the console above this message for any errors and post what you find in the forum: https://forum.getodk.org/"
   exit 1
 fi
 
-echo "starting cron.."
+log "starting cron.."
 cron -f &
 
 MEMTOT=$(vmstat -s | grep 'total memory' | awk '{ print $1 }')
@@ -31,8 +35,8 @@ then
 else
   WORKER_COUNT=1
 fi
-echo "using $WORKER_COUNT worker(s) based on available memory ($MEMTOT).."
+log "using $WORKER_COUNT worker(s) based on available memory ($MEMTOT).."
 
-echo "starting server."
+log "starting server."
 pm2-runtime ./pm2.config.js --instances $WORKER_COUNT
 
