0b106d61552b8dcd04af17d48be6ee6f4876d730
alxndrsn <alxndrsn>
2024-10-06 07:30:21 +0000

    revert unrelated changes

--- files/prebuild/build-frontend.sh
+++ files/prebuild/build-frontend.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -eu
 cd client
-echo "#####################################################"
-ls -al
-echo "#####################################################"
-ls -al node_modules || echo "node_modules not yet populated"
-echo "#####################################################"
 npm clean-install --no-audit --fund=false --update-notifier=false
 npm run build
