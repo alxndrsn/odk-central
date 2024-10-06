1a52f5691c79b54d11e557d04c15a2758ff6d4c7
alxndrsn <alxndrsn>
2024-10-06 05:45:51 +0000

    ci: add debug

--- files/prebuild/build-frontend.sh
+++ files/prebuild/build-frontend.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -eu
 cd client
+echo "#####################################################"
+ls -al
+echo "#####################################################"
+ls -al node_modules || echo "node_modules not yet populated"
+echo "#####################################################"
 npm clean-install --no-audit --fund=false --update-notifier=false
 npm run build
