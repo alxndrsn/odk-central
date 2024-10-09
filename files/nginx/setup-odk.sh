a5c8372cb8ff8b7c1fdec88c509b5c27b8bf5d37
alxndrsn <alxndrsn>
2024-10-09 08:14:37 +0000

    remove debug

--- files/nginx/setup-odk.sh
+++ files/nginx/setup-odk.sh
@@ -26,7 +26,6 @@ if [ "$SSL_TYPE" = "selfsign" ] && [ ! -s "$SELFSIGN_PATH/privkey.pem" ]; then
 fi
 
 CNAME="$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN")"
-echo "CNAME: $CNAME"
 export CNAME="$CNAME"
 
 # start from fresh templates in case ssl type has changed
