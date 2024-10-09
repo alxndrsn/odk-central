438fca387a4e236a82e23abc13275da3d71cd13c
alxndrsn <alxndrsn>
2024-10-09 07:05:25 +0000

    CNAME

--- files/nginx/setup-odk.sh
+++ files/nginx/setup-odk.sh
@@ -25,7 +25,7 @@ if [ "$SSL_TYPE" = "selfsign" ] && [ ! -s "$SELFSIGN_PATH/privkey.pem" ]; then
     -days 3650 -nodes -sha256
 fi
 
-CNAME=$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN") \
+CNAME="$( [ "$SSL_TYPE" = "customssl" ] && echo "local" || echo "$DOMAIN")"
 
 # start from fresh templates in case ssl type has changed
 echo "writing fresh nginx templates..."
