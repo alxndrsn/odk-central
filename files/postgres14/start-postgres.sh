c5b87ea77a2f4d6ab99822d7f1e0261fb052d4ef
alxndrsn <alxndrsn>
2025-07-31 08:41:58 +0000

    log flag loc

--- files/postgres14/start-postgres.sh
+++ files/postgres14/start-postgres.sh
@@ -10,7 +10,7 @@ log() {
 }
 
 if ! [[ -f "$flag_upgradeCompletedOk" ]]; then
-  log "Waiting for upgrade to complete..."
+  log "Waiting for upgrade to complete (flag location: $flag_upgradeCompletedOk)..."
   while ! [[ -f "$flag_upgradeCompletedOk" ]]; do sleep 1; done
   log "Upgrade complete."
 fi
