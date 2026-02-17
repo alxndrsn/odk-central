eabf8b3655b30cd0dc2a7486234e82db11b70cb0
alxndrsn <alxndrsn>
2026-02-17 07:38:37 +0000

    nginx: semgrep

---
+++ test/nginx/semgrep.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eu
+set -o pipefail
+shopt -s inherit_errexit
+
+log() { echo >&2 "[$(basename "$0")] $*"; }
+
+docker_compose() {
+  docker compose --file nginx.test.docker-compose.yml "$@"
+}
+
+lint_service() {
+  local service="$1"
+  log "$service: checking config..."
+  docker_compose exec "$service" bash -euc '
+    # TODO generate .semgrep.yml with a heredoc
+
+    apt update
+    apt install -y python3-venv
+    python3 -m venv .venv
+    . .venv/bin/activate
+    pip install semgrep
+    semgrep scan --metrics=off --disable-version-check --config .semgrep.yml
+  '
+
+  log "$service: config looks OK."
+}
+
+lint_service nginx-ssl-selfsign
+lint_service nginx-ssl-upstream
+
+log "Completed OK."
