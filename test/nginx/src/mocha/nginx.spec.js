c0808a160d54a0906a8948035ff1bd05703c6a11
alxndrsn <alxndrsn>
2026-04-08 07:10:23 +0000

    Revert "without first test"
    This reverts commit 2d338b65e8ca4195003fba16a0d28ef28b98674a.

--- test/nginx/src/mocha/nginx.spec.js
+++ test/nginx/src/mocha/nginx.spec.js
@@ -909,6 +909,21 @@ function standardTestSuite({ fetchHttp, fetchHttp6, apiFetch, apiFetch6, forward
       resetSentryMock(),
     ]));
 
+    it('POST /csp-report should forward requests to Sentry', async () => {
+      // when
+      const res = await apiFetch('/csp-report', {
+        method: 'POST',
+        headers: { 'Content-Type':'application/json' },
+        body: JSON.stringify({ example:1 }),
+      });
+
+      // then
+      assert.equal(res.status, 200);
+      assert.equal(await res.text(), 'OK');
+      // and
+      await assertSentryReceived({ report:{ example:1 } });
+    });
+
     it('POST /csp-report should set original IP for X-Forwarded-For', async () => {
       // when
       const res = await apiFetch('/csp-report', {
