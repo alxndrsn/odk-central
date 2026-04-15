886de577c488c1bf44700d5fafd1d1061478c5a1
alxndrsn <alxndrsn>
2026-04-15 13:33:31 +0000

    mock server: include

--- test/nginx/mock-http-server/index.js
+++ test/nginx/mock-http-server/index.js
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
     'form-action',
     'frame-ancestors',
   ];
-  res.set('Content-Security-Policy',             topLevelDirectives.map(d => `${d} NOTE:FROM-BACKEND:block`)     .join('; '));
-  res.set('Content-Security-Policy-Report-Only', topLevelDirectives.map(d => `${d} NOTE:FROM-BACKEND:reportOnly`).join('; '));
+  res.set('Content-Security-Policy',             'report-uri=bbu; ' + topLevelDirectives.map(d => `${d} NOTE:FROM-BACKEND:block`)     .join('; '));
+  res.set('Content-Security-Policy-Report-Only', 'report-uri=rbu; ' + topLevelDirectives.map(d => `${d} NOTE:FROM-BACKEND:reportOnly`).join('; '));
 
   next();
 });
