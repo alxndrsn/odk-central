89b1a2057ce1de6220da9a41ddd8631aab147c80
alxndrsn <alxndrsn>
2026-09-07 15:44:53 +0000

    add another flag location

--- files/postgres14/start-postgres.sh
+++ files/postgres14/start-postgres.sh
@@ -4,6 +4,7 @@ shopt -s inherit_errexit
 
 flag_upgradeCompletedOk_1="$PGDATA/../.postgres14-upgrade-successful"
 flag_upgradeCompletedOk_2="$PGDATA/.postgres14-upgrade-successful"
+flag_upgradeCompletedOk_3="/postgres14-upgrade/upgrade-successful"
 
 logPrefix="$(basename "$0")"
 log() {
@@ -12,7 +13,8 @@ log() {
 
 log "Checking for flag file at '$flag_upgradeCompletedOk_1' or '$flag_upgradeCompletedOk_2' ..."
 if ! [[ -f "$flag_upgradeCompletedOk_1" ]] &&
-   ! [[ -f "$flag_upgradeCompletedOk_2" ]]; then
+   ! [[ -f "$flag_upgradeCompletedOk_2" ]] &&
+   ! [[ -f "$flag_upgradeCompletedOk_3" ]]; then
   log "Waiting for upgrade to v14 to complete..."
   while ! [[ -f "$flag_upgradeCompletedOk_1" ]]; do sleep 1; done
   touch "$flag_upgradeCompletedOk_2"
