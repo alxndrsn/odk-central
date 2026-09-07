d5f3e18a1224b7d846926f4b224e76ef4d2a6149
alxndrsn <alxndrsn>
2026-09-07 15:15:28 +0000

    more logging

--- files/postgres14/start-postgres.sh
+++ files/postgres14/start-postgres.sh
@@ -9,6 +9,7 @@ log() {
   echo "$(TZ=GMT date) [$logPrefix] $*"
 }
 
+log "Checking for flag file at '$flag_upgradeCompletedOk' ..."
 if ! [[ -f "$flag_upgradeCompletedOk" ]]; then
   log "Waiting for upgrade to complete..."
   while ! [[ -f "$flag_upgradeCompletedOk" ]]; do sleep 1; done
