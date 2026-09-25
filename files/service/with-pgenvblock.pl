37191e209bcc51808171e4ef30a548854a89813e
alxndrsn <alxndrsn>
2026-09-25 06:37:34 +0000

    with-pgenvblock: fix linebreak support

--- files/service/with-pgenvblock.pl
+++ files/service/with-pgenvblock.pl
@@ -1,25 +1,17 @@
-#!/usr/bin/env perl
+#!/bin/bash -eu
+set -o pipefail
 
-use warnings;
-use strict;
+if [[ $# -lt 2 ]]; then
+  echo "Not enough arguments."
+  echo "Usage: $0 [path-to-envblock] [program] [program-arg]..."
+  usage 1
+fi
 
+envFile="$1"
+shift
 
-die "Not enough arguments.\nUsage: with-pgenvblock.pl /path/to/envblock program [program-arg, …]\n" if @ARGV < 2;
-
-my $envblockfile = $ARGV[0];
-
-open my $fh, '<:raw', $envblockfile
-    or die "Cannot open '" . $envblockfile . "': $!";
-my $content = do { local $/; <$fh> };
-close $fh;
-
-my @entries = split /\x00/, $content;
-
-for my $entry (@entries) {
-    if ($entry =~ /\A(?<varname>PG[^=]+)=(?<varvalue>.*)\Z/m) {
-        $ENV{$+{varname}} = $+{varvalue};
-    }
-}
-
-exec @ARGV[1..$#ARGV]
-    or die;
+exec grep --null-data "^PG" "$envFile" |
+    xargs --null \
+    env \
+        --ignore-environment \
+        "$@"
